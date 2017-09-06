3bcef4fb6c2a47b199fda6e20fd86d9fdb2e02f4
JASON <nj.ho@hotmail.com>
2017-09-05 21:32:24 -0600

    first

---
+++ app/src/main/java/journey/android/backgroundlicker/ExampleService.java
@@ -0,0 +1,33 @@
+package journey.android.backgroundlicker;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by Machinatron on 2017-07-14.
+ */
+
+public class ExampleService extends IntentService {
+    private static final String TAG = "EXAMPLESERVICE";
+
+    public static Intent newIntent(Context context){
+        return new Intent(context, ExampleService.class);
+    }
+
+    public ExampleService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+        return super.onStartCommand(intent,flags,startId);
+    }
+}
